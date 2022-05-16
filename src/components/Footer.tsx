e270e77f07a7128934d2424a5feb758f09013e2b
1Spaceone <dotss87@gmail.com>
2022-05-16 19:02:25 +0300

    master

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,15 +6,14 @@ const { Footer } = Layout;
 const { useBreakpoint } = Grid;
 
 const footerElements = [
-  {
-    description: 'Serum Developer Resources',
-    link: helpUrls.developerResources,
-  },
-  { description: 'Discord', link: helpUrls.discord },
+  
+  
   { description: 'Telegram', link: helpUrls.telegram },
+  { description: 'Discord', link: helpUrls.discord },
+  { description: 'Twitter', link: helpUrls.twitter },
   { description: 'GitHub', link: helpUrls.github },
-  { description: 'Project Serum', link: helpUrls.projectSerum },
-  { description: 'Solana Network', link: helpUrls.solanaBeach },
+  
+  
 ];
 
 export const CustomFooter = () => {
--- src/components/HelpUrls.ts
+++ src/components/HelpUrls.ts
@@ -1,11 +1,6 @@
 export const helpUrls = {
-  customerSupport: 'https://t.me/ProjectSerum',
-  customerSupportZh: 'https://t.me/ProjectSerum_Chinese',
-  contactEmail: 'mailto:contact@projectserum.com',
-  discord: 'https://discord.gg/EDvudv6',
-  telegram: 'https://t.me/ProjectSerum',
-  github: 'https://github.com/project-serum',
-  projectSerum: 'https://projectserum.com/',
-  developerResources: 'https://projectserum.com/developer-resources',
-  solanaBeach: 'https://solanabeach.io',
+  discord: 'https://discord.com/invite/xud97tybPc',
+  telegram: 'https://t.me/Onespace_tech',
+  github: 'https://github.com/Onespace-Project',
+  twitter: 'https://twitter.com/1space_me'
 };
